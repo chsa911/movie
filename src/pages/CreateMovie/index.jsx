d984d09f28c31d8ec8ec1ec00ee98110aaea4c70
chsa911 <ch.sa911@gmail.com>
2025-01-08 23:41:34 +0100

    new site for create a book

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -1,1 +1,12 @@
 import styles from "./styles.module.css";
+
+
+function CreateMovie() {
+	const [obj, setObj] = useState({});
+
+<div className="wrapper">
+			<div className="container">
+				<div className="head">
+					<img src="./images/logo.png" alt="logo" className="logo" />
+					<Search setSearch={(search) => setSearch(search)} />
+				</div>
