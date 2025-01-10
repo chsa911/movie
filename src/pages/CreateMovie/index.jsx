fdead69fc69ac305b4f99bc5e5fb9d738c0615b3
chsa911 <ch.sa911@gmail.com>
2025-01-10 22:23:37 +0100

    implement form fields

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -3,6 +3,96 @@ import styles from "./styles.module.css";
 
 function CreateMovie() {
 	const [obj, setObj] = useState({});
+ /*varcomplete*/
+ const [name, setName] = useState('');
+  const [img, setImg] = useState('');
+  const [year, setYear] = useState('');
+  const [genre, setGenre] = useState('');
+  const [rating, setRating] = useState('');
+  const handleSaveBook = () => {
+    const data = {
+   /*varcomplete*/
+      name,
+      img,
+      year,
+      genre,
+      rating,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Book Created successfully', { variant: 'success' });
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+   /*varcomplete*/
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input
+            type='text'
+            value={Img}
+            onChange={(e) => setImg(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='number'
+            value={Year}
+            onChange={(e) => setYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='number'
+            value={Genre}
+            onChange={(e) => setGenre(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input
+            type='number'
+            value={Rating}
+            onChange={(e) => setRating(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+}
 
 <div className="wrapper">
 			<div className="container">
